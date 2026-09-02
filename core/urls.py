756264a8ec1147274565c8ba579c610573a550ba
John Emman Longabela <johnemmanlongabela@gmail.com>
2026-09-02 18:09:08 +0800

    Add generated site HTML and .nojekyll

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,5 @@ from django_distill import distill_path
 from blog import views
 
 urlpatterns = [
-    distill_path('', views.index, name='index'),  # Replace views.index with your home view
-    # Add distill_path for any other static views
+distill_path('', views.post_list, name='post_list')
 ]
