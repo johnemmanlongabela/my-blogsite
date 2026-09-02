7a5df630cb08f694d4dbec8ef450d5c650e4c2cd
John Emman Longabela <johnemmanlongabela@gmail.com>
2026-09-02 19:34:33 +0800

    Fix settings.py syntax error and generate static blog site

--- core/settings.py
+++ core/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / 'staticfiles
+STATIC_ROOT = BASE_DIR / 'staticfiles'
