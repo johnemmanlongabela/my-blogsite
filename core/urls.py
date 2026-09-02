68b3ee699108fc7052a03f492c6a67b6f93c2b8a
John Emman Longabela <johnemmanlongabela@gmail.com>
2026-09-02 19:25:37 +0800

    Update URLs with GitHub Pages project subpath prefix

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,12 @@ from django_distill import distill_path
 from blog import views
 from blog.models import Post
 
-# Function that tells django-distill which post URLs to generate
 def get_all_posts():
     for post in Post.objects.all():
         yield {'slug': post.slug}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    distill_path('', views.post_list, name='post_list'),
-    distill_path('<slug:slug>/', views.post_detail, name='post_detail', distill_func=get_all_posts),
+    distill_path('my-blogsite/', views.post_list, name='post_list'),
+    distill_path('my-blogsite/<slug:slug>/', views.post_detail, name='post_detail', distill_func=get_all_posts),
 ]
