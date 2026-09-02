5e206d6f761a9fd60cd9adc700d906c80ac50fe1
John Emman Longabela <johnemmanlongabela@gmail.com>
2026-09-02 18:47:36 +0800

    Add post_detail route and generate post pages

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,16 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django_distill import distill_path
 from blog import views
+from blog.models import Post
+
+# Function that tells django-distill which post URLs to generate
+def get_all_posts():
+    for post in Post.objects.all():
+        yield {'slug': post.slug}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     distill_path('', views.post_list, name='post_list'),
+    distill_path('<slug:slug>/', views.post_detail, name='post_detail', distill_func=get_all_posts),
 ]
